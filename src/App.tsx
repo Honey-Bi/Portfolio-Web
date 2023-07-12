6c5c9fa148083e15161b177e66d52177ea2ec1b2
Honey-Bi <biten10@naver.com>
2023-07-12 14:37:39 +0900

    project 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Timer from './component/Timer';
-import Main from './component/Main';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
+// import { useRef } from 'react';
+
+// import Timer from './component/Timer';
+
 import './css/index.css';
 import './css/transition.css';
 
-export default function App() {
+import Main from './component/Main';
+import Test from './component/Test';
+import Project from './component/Project';
+
+function App() {
+    const location = useLocation();
+    // const nodeRef = useRef(null);
+    
     return (
-        <BrowserRouter>
-            <Routes>
-            <Route path="/" element={<Main/>} />
-            <Route path="/Timer" element={<Timer s={10}/>} />
-            </Routes>
-        </BrowserRouter>
+        <SwitchTransition mode={"out-in"}>
+            <CSSTransition 
+            // nodeRef={nodeRef} 
+            key={location.key} timeout={800} classNames="move">
+                <Routes location={location}>
+                    <Route path="/" element={<Main/>} />
+                    <Route path="/Project" element={<Project/>} />
+                    <Route path="/Test" element={<Test/>} />
+                </Routes>
+            </CSSTransition>
+        </SwitchTransition>
     )
 }
+export default App;
