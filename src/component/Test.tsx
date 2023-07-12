daee1eaba64fb2bd0be401ba77f09bcbee12f2d9
Honey-Bi <biten10@naver.com>
2023-07-12 14:40:21 +0900

    Create Test.tsx

---
+++ src/component/Test.tsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+
+export default function Test() {
+    return (
+        <div>
+            <Header/>
+            <div id="down">
+                <div className="container">
+                </div>
+            </div>
+        </div>
+    );
+}
