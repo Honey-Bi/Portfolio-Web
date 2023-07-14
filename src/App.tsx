76b9805c508993123657030d42c97c36186a141b
Honey-Bi <biten10@naver.com>
2023-07-14 12:57:21 +0900

    콘셉 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Main from './component/Main';
 import Test from './component/Test';
 import Project from './component/Project';
 import Profile from './component/Profile';
+import Concept from './component/Concept';
 
 function App() {
     const location = useLocation();
@@ -23,8 +24,8 @@ function App() {
                     <Route path="/" element={<Main/>} />
                     <Route path="/Project" element={<Project/>} />
                     <Route path="/Test" element={<Test/>} />
-                    <Route path="/Concept" element={<Test/>} />
                     <Route path="/Making" element={<Test/>} />
+                    <Route path="/Concept" element={<Concept/>} />
                     <Route path="/Profile" element={<Profile/>} />
                 </Routes>
             </CSSTransition>
