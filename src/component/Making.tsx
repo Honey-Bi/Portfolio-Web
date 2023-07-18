d21ce87312bdc17357650a6596270f7df25cc5c6
Honey-Bi <biten10@naver.com>
2023-07-18 17:50:59 +0900

    타임라인 위치 이동

--- src/component/Making.tsx
+++ src/component/Making.tsx
@@ -1,29 +1,10 @@
 import Header from "./Header";
-import { useState } from "react";
+// import { useState } from "react";
 
 import '../css/making.css'
 
 export default function Making() {
-    const [selector, setSelector] = useState<Element|null>(null);
-    const [beforeSame, setBeaforeSave] = useState<boolean>(true);
-    const select = (e:any):void => {
-        let select_element:Element = e.target;
-        if (select_element.classList.contains('color')) {
-            select_element = e.target.parentNode;
-        }
 
-        selector?.classList.remove('active');
-        select_element.classList.add('active');
-
-        if (selector === select_element && beforeSame) {
-            selector?.classList.remove('active');
-            setBeaforeSave(false);
-        } else {
-            setBeaforeSave(true);
-        }
-        setSelector(select_element);
-
-    }
     const toTop = () => {
         const top:Element = document.getElementById('top') as Element;
         top.scrollIntoView(true);
@@ -35,34 +16,7 @@ export default function Making() {
             <Header/>
             <div id="down">
                 <div className="container making-flex">
-                    <div className="time-line">
-                        <div className="line"></div>
-                        <div className="square"></div>
-                        <div className="line-item">
-                            <div className="square selector project" onClick={(e) => select(e)}>
-                                <div className="color"></div>
-                            </div>
-                            <div className="item-title">
-                                
-                            </div>
-                        </div>
-                        <div className="square selector test1" onClick={(e) => select(e)}>
-                            <div className="color"></div>
-                        </div>
-                        <div className="square selector concept" onClick={(e) => select(e)}>
-                            <div className="color"></div>
-                        </div>
-                        <div className="square selector making" onClick={(e) => select(e)}>
-                            <div className="color"></div>
-                        </div>
-                        <div className="square selector profile" onClick={(e) => select(e)}>
-                            <div className="color"></div>
-                        </div>
-                        <div className="square"></div>
-                        <div className="time-line-text">
-                            text
-                        </div>
-                    </div>
+                    
                     <div className="info">
                         <div className="info-project">
                             <div className="info-title">project</div>
--- src/component/Profile.tsx
+++ src/component/Profile.tsx
@@ -5,6 +5,27 @@ import '../css/profile.css';
 
 export default function Profile() {
 
+    const [selector, setSelector] = useState<Element|null>(null);
+    const [beforeSame, setBeaforeSave] = useState<boolean>(true);
+    const select = (e:any):void => {
+        let select_element:Element = e.target;
+        if (select_element.classList.contains('color')) {
+            select_element = e.target.parentNode;
+        }
+
+        selector?.classList.remove('active');
+        select_element.classList.add('active');
+
+        if (selector === select_element && beforeSame) {
+            selector?.classList.remove('active');
+            setBeaforeSave(false);
+        } else {
+            setBeaforeSave(true);
+        }
+        setSelector(select_element);
+
+    }
+
     const [count, setCount] = useState<number>(0);
 
     useEffect(() => {
@@ -31,21 +52,43 @@ export default function Profile() {
     }
 
     return(
-        <div id="main">
+        <div id="main" className="scroll">
             <Header/>
-            <div id="down" className="scrollY">
+            <div id="down">
                 <div className="container">
-                    <div className="profile1">
+                <div className="time-line">
+                        <div className="line"></div>
+                        <div className="square"></div>
+                        <div className="square selector project" onClick={(e) => select(e)}>
+                            <div className="color"></div>
+                        </div>
+                        <div className="square selector test1" onClick={(e) => select(e)}>
+                            <div className="color"></div>
+                        </div>
+                        <div className="square selector concept" onClick={(e) => select(e)}>
+                            <div className="color"></div>
+                        </div>
+                        <div className="square selector making" onClick={(e) => select(e)}>
+                            <div className="color"></div>
+                        </div>
+                        <div className="square selector profile1" onClick={(e) => select(e)}>
+                            <div className="color"></div>
+                        </div>
+                        <div className="square"></div>
+                    </div>
+                    <div className="profile">
                         <div className="names">
                             <div className="name">유현비</div>
                             <div className="name">HyueonBi Yu</div>
                             <div className="name">柳賢飛</div>
                             <div className="name">HB</div>
                         </div>
-                        <p>반갑습니다. 저는 크리에이터이자 개발자입니다. <br/>주로 웹개발을 담당하고 있으며, <span className="accent">항상 새로운 도전</span>을 하려는 자세로 개발을 하고 있습니다.</p>
+                        {/* <p>반갑습니다. 저는 크리에이터이자 개발자입니다. <br/>주로 웹개발을 담당하고 있으며, <span className="accent">항상 새로운 도전</span>을 하려는 자세로 개발을 하고 있습니다.</p> */}
                         
                     </div>
+                    
                 </div>
+                <div className="container"></div>
             </div>
         </div>
     )
