28b606811d62f205c2fae6b1140e15ea5a9c3453
Honey-Bi <biten10@naver.com>
2023-07-25 09:18:20 +0900

    콘셉트 페이지 재생성

--- src/component/Concept.tsx
+++ src/component/Concept.tsx
@@ -1,7 +1,19 @@
+import Header from './Header';
 import '../css/concept.css';
 
 export default function Concept() {
     return (
+        <div id='main'>
+        <Header/>
+        <div id="down">
+            <div className="wrap">
+                <div className="concept">
+                    나는
+                    <span className="im">asd</span>
+                    입니다.
+                </div>
             </div>
         </div>
+    </div>
     )
+}
