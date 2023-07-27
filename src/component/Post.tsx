b99378bdd49d15fa15bc9f912742d70873d20439
Honey-Bi <biten10@naver.com>
2023-07-27 17:50:26 +0900

    Update Post.tsx

--- src/component/Post.tsx
+++ src/component/Post.tsx
@@ -1,34 +1,53 @@
 
-import { Link, useLocation } from "react-router-dom";
-
+import { Link, useLocation, useParams } from "react-router-dom";
 import '../css/post.css'
+import ToTop from "./ToTop";
+import Post1 from "./post/Test";
+import { useEffect, useState } from "react";
+import Post2 from "./post/Test2";
 
 export default function Post() {
-
     const location = useLocation();
     const color = location.state.color;
-    const project_enter = document.getElementsByClassName('project-enter')[0];
-    if (project_enter) {
-        project_enter.remove();
-    }
+    const { pname } = useParams();
+    const project_enter:Element|null = document.getElementsByClassName('project-enter')[0];
+    if (project_enter) project_enter.remove();
+
+    const [post, setPost] = useState<JSX.Element|null>(null);
+    const [once, setOnce] = useState<boolean>(true);
+
+    useEffect(() => {
+        const project_post:Element|null = document.getElementsByClassName('post-transition')[0];
+        if (project_post && once) {
+            setOnce(false);
+            switch(pname) {
+                case 'test':
+                    setPost(Post1());
+                    break;
+                case 'test2':
+                    setPost(Post2());
+                    break;
+            }
+        };
+    }, [once, pname]);
     return(
         <div 
-            id="main" 
+            id="main" className="scroll"
             style={{
                 backgroundColor: color
             }}
-        >   
-            <div className="project-nav">
+        >
+            <div id="top"></div>
+            <div className="project-nav"
+            style={{
+                backgroundColor: color
+            }}>
                 <Link to={"/"} className="project-home" />
                 <Link to={"/project"} className="project-exit" />
             </div>
-            <div className="container post-transition">
-                <div className="post">
-                    <div className="post-title">제목</div>
-                    <div className="post-body">
-                        내용
-                    </div>
-                </div>
+            <div className="container post-transition"> 
+                {post}
+                <ToTop color='#fff'/>
             </div>
         </div>
     )
