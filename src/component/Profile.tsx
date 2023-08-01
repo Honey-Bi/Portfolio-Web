acbef8bded83cd50d34e17dfd45c5d4d89b5d61d
Honey-Bi <biten10@naver.com>
2023-08-01 17:51:38 +0900

    Update Profile.tsx

--- src/component/Profile.tsx
+++ src/component/Profile.tsx
@@ -1,14 +1,16 @@
 import Header from "./Header";
-import { useState } from "react";
+import { BaseSyntheticEvent, useState } from "react";
 
 import 'css/profile.css';
 import Names from "./Names";
+import ToTop from "./ToTop";
 
 export default function Profile() {
 
     const [selector, setSelector] = useState<Element|null>(null);
     const [beforeSame, setBeaforeSave] = useState<boolean>(true);
-    const select = (e:any):void => {
+
+    const select = (e:BaseSyntheticEvent, id:string):void => {
 
         let select_element:Element = e.target;
         if (select_element.classList.contains('color')) {
@@ -19,48 +21,93 @@ export default function Profile() {
         select_element.classList.add('active');
 
         if (selector === select_element && beforeSame) {
-            selector?.classList.remove('active');
+            selector.classList.remove('active');
             setBeaforeSave(false);
         } else {
             setBeaforeSave(true);
         }
         setSelector(select_element);
 
+        const move = document.getElementById(id);
+        move?.scrollIntoView({behavior:"smooth", block:"center"});
     }
-
+    const removeSelect = (e:BaseSyntheticEvent) => {
+        if (selector) selector.classList.remove('active');
+    };
     
 
     return(
         <div id="main" className="scroll">
             <Header/>
-            <div id="down">
-                <div className="container">
+            <div id="top"></div>
+            <div id="down"
+                onWheel={removeSelect}
+                onTouchMove={removeSelect}
+            >
+                <div className="container" id="profile">
                     <div className="time-line">
                         <div className="line"></div>
                         <div className="square"></div>
-                        <div className="square selector project" onClick={(e) => select(e)}>
+                        <div className="square selector about" onClick={(e) => select(e, 'about')}>
                             <div className="color"></div>
                         </div>
-                        <div className="square selector test1" onClick={(e) => select(e)}>
+                        <div className="square selector test1" onClick={(e) => select(e, '')}>
                             <div className="color"></div>
                         </div>
-                        <div className="square selector concept" onClick={(e) => select(e)}>
+                        <div className="square selector concept" onClick={(e) => select(e, '')}>
                             <div className="color"></div>
                         </div>
-                        <div className="square selector making" onClick={(e) => select(e)}>
+                        <div className="square selector making" onClick={(e) => select(e, '')}>
                             <div className="color"></div>
                         </div>
-                        <div className="square selector profile1" onClick={(e) => select(e)}>
+                        <div className="square selector skills" onClick={(e) => select(e, 'skills')}>
                             <div className="color"></div>
                         </div>
                         <div className="square"></div>
                     </div>
                     <div className="profile">
-                        <Names/>
-                        {/* <p>반갑습니다. 저는 크리에이터이자 개발자입니다. <br/>주로 웹개발을 담당하고 있으며, <span className="accent">항상 새로운 도전</span>을 하려는 자세로 개발을 하고 있습니다.</p> */}
-                        
+                        <div className="profile-about">
+                            <div className="profile-sub" id="about">about me</div>
+                            <div className="profile-name">
+                                <div className="name-hidden">&nbsp;</div>
+                                <Names/>
+                            </div>
+                            {/* <p>반갑습니다. 저는 크리에이터이자 개발자입니다. <br/>주로 웹개발을 담당하고 있으며, <span className="accent">항상 새로운 도전</span>을 하려는 자세로 개발을 하고 있습니다.</p> */}
+                            <div className="profile-contact">
+                                <span>EMAIL : <a href="mailto:biten10@naver.com">biten10@naver.com</a></span>
+                                <a href="https://github.com/Honey-Bi" target="_blink">
+                                    <i className="fa-brands fa-github"></i>
+                                    github
+                                </a>
+                            </div>
+                            
+                        </div>
+                        <div className="dummy"></div>
+                        <div className="profile-skills">
+                            <div className="profile-sub" id="skills">skills</div>
+                            <ul className="skill_list">
+                                <li>FrontEnd
+                                    <ul className="skill_down">
+                                        <li>TypeScript</li>
+                                        <li>Jquery</li>
+                                        <li>React</li>
+                                    </ul>
+                                </li>
+                                <li>BackEnd
+                                    <ul className="skill_down">
+                                        <li>Express</li>
+                                        <li>MongoDB</li>
+                                    </ul>
+                                </li>
+                                <li>Data
+                                    <ul className="skill_down">
+                                        <li>SQL</li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                    
+                    <ToTop color='#fff'/>
                 </div>
                 <div className="profile-title">
                     profile
