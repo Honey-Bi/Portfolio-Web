dac10ea573a0d53bd8ed134b28ccb5cb891164de
Honey-Bi <biten10@naver.com>
2023-08-01 17:51:29 +0900

    Update Post.tsx

--- src/component/Post.tsx
+++ src/component/Post.tsx
@@ -1,6 +1,6 @@
 
 import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState, useCallback } from 'react';
 import 'css/post.css'
 import ToTop from "./ToTop";
 import posts from'post.json';
@@ -24,6 +24,11 @@ export default function Post() {
         content: string;
         class: Array<string>;
     }
+    type RowData = {
+        type: string;
+        class: Array<string>;
+        body: Array<ImgData|TextData>;
+    }
     type PostData = {
         title: string;
         category: string;
@@ -31,8 +36,53 @@ export default function Post() {
         date: string;
         color: string;
         tColor: string;
-        body: Array<TextData|ImgData>;
+        body: Array<TextData|ImgData|RowData>;
     }
+
+    const add_img = useCallback((data:ImgData, body:HTMLDivElement) => {
+        const img = document.createElement('img');
+        img.setAttribute('src',  require(`img/${data.src}`));
+        img.setAttribute('alt', data.alt);
+        for (let c of data.class) img.classList.add(c);
+        body.appendChild(img);
+    }, []);
+    const add_text = useCallback((data:TextData, body:HTMLDivElement) => {
+        let text:Element;
+        if (data.link) { //하이퍼 링크 생성
+            text = document.createElement('a');
+            text.setAttribute('href', data.href);
+            text.setAttribute('target', '_blank');
+        } else if (data.bold) {
+            text = document.createElement('b');
+        } else {
+            text = document.createElement('p');
+        }
+        for (let c of data.class) {text.classList.add(c)};
+        
+        text.textContent = data.content;
+        body.appendChild(text);
+    }, []);
+
+    const add_item = useCallback((data:ImgData|TextData|RowData, body:HTMLDivElement) => {
+        switch (data.type) {
+            case "img":
+                add_img(data as ImgData, body);
+                break;
+            case "text":
+                add_text(data as TextData, body);
+                break;
+            case "row":
+                const rowData = data as RowData;
+                const row = document.createElement('div');
+                row.classList.add('row');
+                for (let c of data.class) {row.classList.add(c)};
+                body.appendChild(row);
+                for(let i of rowData.body) {
+                    add_item(i, row);
+                }
+                break;
+        }
+    }, [add_img, add_text])
 
     const [data, setData] = useState<PostData|null>(null);
     const color:string = (location.state) ? location.state.color : data?.color;
@@ -43,34 +93,10 @@ export default function Post() {
             const body = bodyRef.current;
             body.textContent = '';
             for (var i of data.body) {
-                if(i.type === "img") { // 이미지 생성
-                    const data = i as ImgData; 
-                    const img = document.createElement('img');
-                    img.setAttribute('src',  require(`img/${data.src}`));
-                    img.setAttribute('alt', data.alt);
-                    for (let c of data.class) img.classList.add(c);
-                    body.appendChild(img);
-                    continue;
-                }
-
-                const data = i as TextData; // 글자 생성
-                let text:Element;
-                if (data.link) { //하이퍼 링크 생성
-                    text = document.createElement('a');
-                    text.setAttribute('href', data.href);
-                    text.setAttribute('target', '_blank');
-                } else if (data.bold) {
-                    text = document.createElement('b');
-                } else {
-                    text = document.createElement('p');
-                }
-                for (let c of data.class) {text.classList.add(c)};
-                
-                text.textContent = data.content;
-                body.appendChild(text);
+                add_item(i, body);
             }
         }
-    }, [bodyRef, data, navigate, pname]);
+    }, [add_item, bodyRef, data, navigate, pname]);
 
     const getTitle = ():string => {
         if (data) return data.title;
