1f95550d17b4c658c65fa918246d141a390e631a
Honey-Bi <biten10@naver.com>
2023-08-07 14:44:35 +0900

    깃허브 페이지 spa 처리

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,10 @@ import Project from './component/Project';
 import Profile from './component/Profile';
 import Concept from './component/Concept';
 import Terminal from './component/Terminal';
-// import NotFound404 from './component/404';
 import Trees from 'component/Trees';
 
 function App() {
     const location = useLocation();
-    // const nodeRef = useRef();
     let className:string; 
         
     switch (true) {
@@ -33,7 +31,6 @@ function App() {
     return (
         <SwitchTransition mode={"out-in"}>
             <CSSTransition
-                // nodeRef={nodeRef} 
                 key={location.key} timeout={800} classNames={className}>
                 <Routes location={location}>
                     <Route path="/" element={<Main />} />
@@ -43,7 +40,6 @@ function App() {
                     <Route path="/terminal" element={<Terminal />} />
                     <Route path="/profile" element={<Profile />} />
                     <Route path="/test/tree" element={<Trees />}  />
-                    {/* <Route path="/*" element={<NotFound404 />} /> */}
                 </Routes>
             </CSSTransition>
         </SwitchTransition>
--- src/component/404.tsx
+++
@@ -1,9 +0,0 @@
-import { useLocation } from "react-router-dom";
-
-export default function NotFound404() {    
-    const location = useLocation();
-    if (location.pathname !== '/404') window.location.replace('/404');
-    return (
-        <div className="404">404 not found</div>
-    )
-}
