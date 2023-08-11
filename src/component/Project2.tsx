cdc31cc1708ee7f7ba5ab98d4065a4700684f44c
Honey-Bi <biten10@naver.com>
2023-08-11 12:50:33 +0900

    프로젝트 리스트 디자인 변경

--- src/component/Project2.tsx
+++ src/component/Project2.tsx
@@ -1,8 +1,44 @@
+import Header from './Header';
+import style from 'css/project2.module.css';
+import posts from'json/post.json';
+
 function Project2() {
+    const render_project_list = ():JSX.Element => {
+        let result = [];
+        let row = [];
+        let count = 0;
+        for (var i of Object.values(posts)) {
+            const src = Object.keys(posts)[count];
+            if (count % 2 === 0) row= [];
+            row.push(
+                <div key={src} className={style.project} style={{backgroundColor: i.color}}>
+                    <div style={{color: i.tColor}} className={style.project_date}>{i.date}</div>
+                    <div style={{color: i.tColor}} className={style.project_title}>{i.title}</div>
+                    <div style={{color: i.tColor}} className={style.project_category}>{i.category}</div>
+                </div>
+            );
+            if (count % 2 === 0) {
+                result.push(<div key={count} className={style.row}>{row}</div>)
+            }
+            count++;
+        }
+        return (
+            <div className={`project-list ${style.project_list}`}>
+                {result}
+            </div>
+        );
+    }
+
     return (
-        <div className=""></div>
+        <div id="main">
+            <Header />
+            <div id="down" className="down-height scroll">
+                {render_project_list()}
+            </div>
+            <div className="project-title">
+                project
+            </div>
+        </div>
     )
-
 }
-
 export default Project2;
