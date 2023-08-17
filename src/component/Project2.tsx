ec3700ab72132d3775a9982aa1e9c7f2543cb71b
Honey-Bi <biten10@naver.com>
2023-08-17 17:51:06 +0900

    레코드 커버 디자인 수정

--- src/component/Project2.tsx
+++ src/component/Project2.tsx
@@ -1,13 +1,17 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useCallback } from "react";
+import { useNavigate } from 'react-router-dom';
 import Header from "./Header";
 import 'css/project2.css';
 import posts from "json/post.json"
+import { Link } from "react-router-dom";
+import Github from "./github";
 
 const golden_ratio = 1.61803398875; // 황금비
 
 function Project4() {
     const baseRef = useRef<HTMLDivElement>(null);
     const viewRef = useRef<HTMLDivElement>(null);
+    const navigate = useNavigate();
     type Size = {
         width: number,
         height: number,
@@ -68,7 +72,25 @@ function Project4() {
         if (deep === depth) return;
         setDepth(deep);
     }
+
     type flexDirection = "column" | "row-reverse" | "column-reverse" | "row";
+
+    const move = useCallback((e:React.MouseEvent, pName:string, bgColor:string) => { // 페이지 이동 함수
+        const main:Element = document.getElementById('main') as Element;
+        const style = `
+            top: -${document.body.clientHeight - e.pageY}px;
+            left: -${document.body.clientWidth - e.pageX}px;
+            background-color: ${bgColor}
+        `;
+        const add_element = document.createElement('div');
+        add_element.classList.add('project-enter');
+        add_element.setAttribute('style', style);
+        main.after(add_element)
+
+        navigate(`./post/${pName}`,{
+            state: { color: bgColor },
+        });
+    }, [navigate])
 
     function renderProject(min:number, max:number, count:number): JSX.Element {
         if (count === posts.length+1) {
@@ -101,20 +123,29 @@ function Project4() {
                 <div className="project-scale" 
                 style={{width: size.min, height: size.min,
                         transform: `scale(${scale})`,
-                        // transformOrigin: `${min/2} ${min/2} `
                     }}>
                     <div className="project-content"
                     style={{
                         backgroundColor: data.color,
                         transform: `rotate(${rotate}deg)`
                     }}>
-                        <div className="project-title" style={{color: data.tColor}}>
-                            {data.title}
+                        <div className="project-title" style={{color: data.tColor}}>{data.title}</div>
+                        <span className="project-date">{data.date}</span>
+                        <span className="project-author"> - {data.author}</span>
+                        <div className="project-about" onClick={e => move(e, data.title, data.color)}>
+                            <div className="back"></div>
+                            <span>about</span>
+                        </div>
+                        <div className="project-link">
+                            <a href="https://github.com/Honey-Bi/Honey-Bi.github.io" target="_blank" rel="noreferrer">
+                                <Github color="#fff" size="24"/>
+                                github
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className={`project-list ${count} ${count < depth?active[index%4]:''}`}
+            <div className={`project-list ${count < depth?active[index%4]:''}`}
                 style={{
                     width: width, height:height, 
                     flexDirection: direction[index % 4]}
@@ -151,19 +182,53 @@ function Project4() {
         tabIndex={0}
         onKeyDown={e =>handleKeyDown(e)}>
         <Header />
-        <div id="down" className='down-height base' 
+        <div id="down" className='down-height project' 
             ref={baseRef} 
             onWheel={moveWheel}>
-            <div className="back-button" onClick={goBack}></div>
+            <div className="back-button">
+                <svg 
+                    onClick={goBack}
+                    xmlns="http://www.w3.org/2000/svg" 
+                    viewBox="0 0 72.00 72.00" 
+                    enableBackground="new 0 0 72 72" 
+                    strokeWidth="0.00072" 
+                    transform="matrix(1, 0, 0, -1, 0, 0)rotate(0)">
+                        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke="#CCCCCC" strokeWidth="1.584"></g>
+                        <g id="SVGRepo_iconCarrier"> 
+                            <g> 
+                                <path d="M48.252,69.253c-2.271,0-4.405-0.884-6.011-2.489L17.736,42.258c-1.646-1.645-2.546-3.921-2.479-6.255 c-0.068-2.337,0.833-4.614,2.479-6.261L42.242,5.236c1.605-1.605,3.739-2.489,6.01-2.489c2.271,0,4.405,0.884,6.01,2.489 c3.314,3.314,3.314,8.707,0,12.021L35.519,36l18.743,18.742c3.314,3.314,3.314,8.707,0,12.021 C52.656,68.369,50.522,69.253,48.252,69.253z M48.252,6.747c-1.202,0-2.332,0.468-3.182,1.317L21.038,32.57 c-0.891,0.893-0.833,2.084-0.833,3.355c0,0.051,0,0.101,0,0.151c0,1.271-0.058,2.461,0.833,3.353l24.269,24.506 c0.85,0.85,1.862,1.317,3.063,1.317c1.203,0,2.273-0.468,3.123-1.317c1.755-1.755,1.725-4.61-0.03-6.365L31.292,37.414 c-0.781-0.781-0.788-2.047-0.007-2.828L51.438,14.43c1.754-1.755,1.753-4.61-0.001-6.365C50.587,7.215,49.454,6.747,48.252,6.747z"></path>
+                            </g>
+                        </g>
+                    </svg>
+            </div>
             <div className="project-container" style={{width: size.width,height:size.height}}>
                 <div className="project-view" ref={viewRef}>
-                    <div className={`project-item ${0 < depth?'active':''}`} 
+                    <div className={`project-item disabled ${0 < depth?'active':''}`} 
                         style={{width: size.min, height:size.min}}
                         onClick={e => select(e, 0)}>
                             <div className="project-scale"
                                 style={{width: size.min, height: size.min}}>
                                 <div className="project-content">
                                     <div className="project-title">Project</div>
+                                    <div className="project-guide">
+                                        <div className="row">
+                                            <div className="key">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="19" x2="12" y2="5"></line><polyline points="5 12 12 5 19 12"></polyline></svg>
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="key">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>
+                                            </div>
+                                            <div className="key">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><polyline points="19 12 12 19 5 12"></polyline></svg>
+                                            </div>
+                                            <div className="key">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                     </div>
